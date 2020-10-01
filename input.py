8cc0bcab991a87c5e7affe1aae7aef22622387a7
Sushant Gautam <susant.gautam@gmail.com>
2020-10-01 13:24:31 +0545

    update fitness function with 'x'

--- input.py
+++ input.py
@@ -1,4 +1,4 @@
-def fitness_function(gene):
-    # Just Modify this for fitness function change. Index of 'gene' is same as the "BOUNDS" value in input.json file.
-    # gene[0] refers to first set of [upper, lower bound] and so on.
-    return gene[0] * gene[0] - gene[1] * gene[0] + gene[1] * gene[2]
+def fitness_function(x):
+    # Just Modify this for fitness function change. Index of 'x' is same as the "BOUNDS" value in input.json file.
+    # x[0] refers to first set of [upper, lower bound] and so on.
+    return x[0] * x[0] - x[1] * x[0] + x[1] * x[2]
