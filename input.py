c8f2512dbc794ce700672063c0037a42108d61c6
Sushant Gautam <susant.gautam@gmail.com>
2020-10-01 13:02:40 +0545

    fitness_function comment

--- input.py
+++ input.py
@@ -1,3 +1,4 @@
 def fitness_function(gene):
-    #Just Modify this for fitness function change.
-    return gene[0] * gene[0] - gene[1] * gene[0] + gene[1]* gene[2]
+    # Just Modify this for fitness function change. Index of 'gene' is same as the "BOUNDS" value in input.json file.
+    # gene[0] refers to first set of [upper, lower bound] and so on.
+    return gene[0] * gene[0] - gene[1] * gene[0] + gene[1] * gene[2]
