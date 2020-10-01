a7e9d3cb789fd3340776f1665976c261a3098e6e
Sushant Gautam <susant.gautam@gmail.com>
2020-10-01 10:35:31 +0545

    code update

--- input.py
+++ input.py
@@ -1,3 +1,3 @@
 def fitness_function(gene):
     #Just Modify this for fitness function change.
-    return gene[0] ** gene[0] + gene[1] * gene[0] + gene[1] * gene[2]
+    return gene[0] * gene[0] + gene[1] * gene[0] + gene[1] * gene[2]
--- main.py
+++ main.py
@@ -1,88 +1,68 @@
 import json
 import statistics
 from copy import deepcopy
-from random import seed, random, sample, randrange, randint
+from random import random, sample, randrange, randint, seed
 
 import numpy as np
-import pandas as pd
 
 from input import fitness_function
 
 
-def GeneticAlgorithm(name):
-    generation = 0
+def GeneticAlgorithm():
+    # read input params from file
     with open('input.json') as json_file:
-        input= json.load(json_file)
-
+        input = json.load(json_file)
+    # Initialize Population from given parameters
     population = Population(input=input)
-
+    # Also Evaluate fitness and store the best parameter
     population.Evaluate()
     population.keep_the_best()
     print('\nOperation Running.')
-
+    # loop until MAXGENS
+    generation = 0  # counter
     while (generation < input['MAXGENS']):
         print("\n## Generation:", generation)
         population.Selection()
         population.Crossover()
         population.Mutate()
-        population.Report()
+        population.Report(generation)
         population.Evaluate()
         population.Elitist()
-        generation += 1
-
-    df = pd.DataFrame(statLog)
+        generation += 1  # counter update
 
     print('\nOperation Completed.')
-    print(df)
+    print(np.array(statLog), )  # print log
     print('The best solution is ', population.best['bestFitChromo'])
 
 
-statLog = []
-seed(2)
-input = None
+statLog, input = [], None
+seed(2)  # random seed value
 
 
 class Chromosome:
     def __init__(self, BOUNDS):
+        self.gene = []  # init gene variable
         NVAR = len(BOUNDS)
-        self.gene = []
         for i in range(NVAR):
             lower, upper = BOUNDS[i]
-            self.gene.append(lower + (upper - lower) * random())
+            self.gene.append(lower + (upper - lower) * random())  # random gene generation within boundary
 
     def mutate(self, BOUNDS):
         m = randrange(0, len(BOUNDS))
         lower, upper = BOUNDS[m]
-        self.gene[m] = lower + (upper - lower) * random()
+        self.gene[m] = lower + (upper - lower) * random()  # gene mutation to random bound value
         print("Mutate ", m, " now ", self.gene)
 
-    def __repr__(self):
+    def __repr__(self):  # to display the object
         return repr(self.gene)
 
 
 class Population:
-    def __init__(self, input=None, fitness=0, chromosomes=None, ):
-        self.bounds = input['BOUNDS']
-        self.fitness = fitness
-        self.popsize = input['POPSIZE']
+    def __init__(self, input=None, ):
         self.input = input.copy()
-        self.best = None
-
-        if chromosomes:
-            self.chromosomes = chromosomes
-        else:
-            self.chromosomes = []
-            # no chromosome given so generate
-            for i in range(self.popsize):
-                self.chromosomes.append(Chromosome(self.bounds))
-
-    # return as array for visulization
-    def __repr__(self):
-        return repr(np.array([e for e in self.chromosomes]))
-
-    @property
-    def array(self):
-        return np.array([e for e in self.chromosomes]).reshape((self.popsize, -1))
+        self.bounds, self.popsize = input['BOUNDS'], input['POPSIZE']
+        self.chromosomes = [Chromosome(self.bounds) for i in range(self.popsize)]  # random generate chromosome
+        self.fitness, self.best = None, None
 
     def keep_the_best(self):
         self.best = {"bestFitnessVal": max(self.fitness),
@@ -96,11 +76,11 @@ class Population:
         totalFitness = sum(self.fitness)
         rfitness = np.array(self.fitness) / totalFitness
         cfitness = np.cumsum(rfitness)
-        oldChromosomes = deepcopy(self.chromosomes)
+        oldChromosomes = deepcopy(self.chromosomes)  # save old state of population chromosomes
         for i in range(self.popsize):
-            randomNumber = random()
-            justGreaterThan = next(i for i, v in enumerate(cfitness) if v >= randomNumber)
-            self.chromosomes[i] = deepcopy(oldChromosomes[justGreaterThan])
+            justGreaterThan = next(
+                i for i, v in enumerate(cfitness) if v >= random())  # just greater than a random num between 0 and 1
+            self.chromosomes[i] = deepcopy(oldChromosomes[justGreaterThan])  # selected
             print('selected ', justGreaterThan, ' at ', i)
 
     def Crossover(self):
@@ -109,16 +89,14 @@ class Population:
         XOVER_Popn = sample(range(0, self.popsize), XOVER_num)  # select population for crossover
         # improvement can be done with more than two candidate for crossover
         print("XOVER_Popn: ", XOVER_Popn, " Len: ", len(XOVER_Popn))
-        old = deepcopy([(x.gene) for x in self.chromosomes])
+        old = deepcopy([(x.gene) for x in self.chromosomes])  # save old state of population chromosomes
         for x in XOVER_Popn:
             while True:
                 y = randint(0, XOVER_num)
                 if x != y: break
             # select  crossover  point
             b = randrange(0, len(self.bounds))
-            xm, my = x, y
-            xc, cx = self.chromosomes[x].gene[b], old[y][b]
-            self.chromosomes[x].gene[b] = old[y][b]
+            self.chromosomes[x].gene[b] = old[y][b]  # crossover
             print('Cross ', x, "-", y, "@", b)
 
     def Mutate(self):
@@ -128,14 +106,6 @@ class Population:
         for x in MUTATION_Popn:
             self.chromosomes[x].mutate(self.bounds)
 
-    def Report(self):
-        stdev = statistics.stdev(self.fitness)
-        # print("BestVal", self.best['bestFitnessVal'], " stdev ", stdev, " fit ", self.best)
-        statLog.append({"bestVal": self.best['bestFitnessVal'],
-                        " stdev ": stdev,
-                        " fit ": self.best['bestFitChromo']
-                        })
-
     def Elitist(self):
         old = deepcopy(self.best)
         best = {"bestFitnessVal": max(self.fitness),
@@ -144,17 +114,26 @@ class Population:
 
         if best['bestFitnessVal'] < old['bestFitnessVal']:
             worst = self.fitness.index(min(self.fitness))
-            self.chromosomes[worst] = old['bestFitChromo']
-            self.fitness[worst] = old['bestFitnessVal']
-            self.keep_the_best()
+            self.chromosomes[worst] = old['bestFitChromo']  # perform elitism
+            self.fitness[worst] = old['bestFitnessVal']  # also update fitness value
             print('Elitism Performed for ', old['bestFitChromo'], ' FitnessVal: ', old['bestFitnessVal'])
-            pass
-
         else:
-            self.keep_the_best()
             print('Elitism Not Performed')
-            pass
+        self.keep_the_best()  # select and store the best of all
+
+    def Report(self, generation):
+        stdev = statistics.stdev(self.fitness)
+        # print("BestVal", self.best['bestFitnessVal'], " stdev ", stdev, " fit ", self.best)
+        statLog.append({"Generation": generation,
+                        "BestFit": self.best['bestFitnessVal'],
+                        "std": stdev,
+                        "chromosome": self.best['bestFitChromo']
+                        })
+
+    @property  # just for better display
+    def array(self):
+        return np.array([e for e in self.chromosomes]).reshape((self.popsize, -1))
 
 
 if __name__ == '__main__':
-    GeneticAlgorithm('GA')
+    GeneticAlgorithm()
