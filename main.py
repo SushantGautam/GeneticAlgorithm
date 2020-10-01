8b6712fa4973096dc9257e7f1d23ed93712f5d29
Sushant Gautam <susant.gautam@gmail.com>
2020-10-01 10:44:33 +0545

    code update

--- main.py
+++ main.py
@@ -40,16 +40,16 @@ seed(2)  # random seed value
 
 
 class Chromosome:
-    def __init__(self, BOUNDS):
-        self.gene = []  # init gene variable
-        NVAR = len(BOUNDS)
-        for i in range(NVAR):
-            lower, upper = BOUNDS[i]
+    def __init__(self, BOUNDS):  # constructor to init random genes
+        self.gene = []
+        NVAR = len(BOUNDS)  # find number of variables/genes
+        for i in range(NVAR):  # generate random value for all gene variables
+            lower, upper = BOUNDS[i]  # proper boundary for all variables
             self.gene.append(lower + (upper - lower) * random())  # random gene generation within boundary
 
     def mutate(self, BOUNDS):
         m = randrange(0, len(BOUNDS))
-        lower, upper = BOUNDS[m]
+        lower, upper = BOUNDS[m]  # proper boundary for  variable
         self.gene[m] = lower + (upper - lower) * random()  # gene mutation to random bound value
         print("Mutate ", m, " now ", self.gene)
 
@@ -58,18 +58,18 @@ class Chromosome:
 
 
 class Population:
-    def __init__(self, input=None, ):
+    def __init__(self, input=None, ):  # constructor to generate random population
         self.input = input.copy()
         self.bounds, self.popsize = input['BOUNDS'], input['POPSIZE']
-        self.chromosomes = [Chromosome(self.bounds) for i in range(self.popsize)]  # random generate chromosome
-        self.fitness, self.best = None, None
+        self.chromosomes = [Chromosome(self.bounds) for i in range(self.popsize)]  # random chromosome generation
+        self.fitness, self.best = None, None  # declare only for future use
 
-    def keep_the_best(self):
+    def keep_the_best(self):  # save best value to population class
         self.best = {"bestFitnessVal": max(self.fitness),
                      "bestFitChromo": self.chromosomes[self.fitness.index(max(self.fitness))]
                      }
 
-    def Evaluate(self, ):
+    def Evaluate(self):  # evaluate/calculate fitness of all chromosomes
         self.fitness = [fitness_function(chromosome.gene) for chromosome in self.chromosomes]
 
     def Selection(self):
@@ -84,8 +84,8 @@ class Population:
             print('selected ', justGreaterThan, ' at ', i)
 
     def Crossover(self):
-        # find number of population to go for crossover
-        XOVER_num = int(self.input['PXOVER'] * self.popsize)
+
+        XOVER_num = int(self.input['PXOVER'] * self.popsize)  # number of population to go for crossover
         XOVER_Popn = sample(range(0, self.popsize), XOVER_num)  # select population for crossover
         # improvement can be done with more than two candidate for crossover
         print("XOVER_Popn: ", XOVER_Popn, " Len: ", len(XOVER_Popn))
@@ -100,21 +100,21 @@ class Population:
             print('Cross ', x, "-", y, "@", b)
 
     def Mutate(self):
-        MUTATION_num = int(self.input['PMUTATION'] * self.popsize)
-        MUTATION_Popn = sample(range(0, self.popsize), MUTATION_num)
+        MUTATION_num = int(self.input['PMUTATION'] * self.popsize)  # number of population to go for mutation
+        MUTATION_Popn = sample(range(0, self.popsize), MUTATION_num)  # select population for mutation
         print("Mutating ", MUTATION_Popn, ' len ', MUTATION_num)
         for x in MUTATION_Popn:
-            self.chromosomes[x].mutate(self.bounds)
+            self.chromosomes[x].mutate(self.bounds)  # mutate within boundary
 
     def Elitist(self):
-        old = deepcopy(self.best)
+        old = deepcopy(self.best)  # save old state of population
         best = {"bestFitnessVal": max(self.fitness),
                 "bestFitChromo": self.chromosomes[self.fitness.index(max(self.fitness))]
-                }
+                }  # calculate best of current population
 
-        if best['bestFitnessVal'] < old['bestFitnessVal']:
-            worst = self.fitness.index(min(self.fitness))
-            self.chromosomes[worst] = old['bestFitChromo']  # perform elitism
+        if best['bestFitnessVal'] < old['bestFitnessVal']:  # compare bests
+            worst = self.fitness.index(min(self.fitness))  # find worst of current
+            self.chromosomes[worst] = old['bestFitChromo']  # perform elitism over worst
             self.fitness[worst] = old['bestFitnessVal']  # also update fitness value
             print('Elitism Performed for ', old['bestFitChromo'], ' FitnessVal: ', old['bestFitnessVal'])
         else:
